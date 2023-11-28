e4b5bf17b4ddf505adf61ca9a49639da0b69da45
001alisson <149392416+001alisson@users.noreply.github.com>
2023-11-28 20:59:53 -0300

    Add files via upload

---
+++ stj.js
@@ -0,0 +1,156 @@
+function c1(){
+    document.getElementById("imc").src="Ada_Lovelace.png";
+    var nome = "Ada Lovelace"
+    document.getElementById("nc").innerHTML = nome;
+    
+    var bio = "A britânica Ada Lovelace foi a primeira programadora da história. Ela viveu por apenas 36 anos, até 1852, mas foi ela quem previu que os computadores fariam mais do que simples cálculos matemáticos, criando o primeiro algoritmo capaz de ser processado por uma máquina.";
+    document.getElementById("sobc").innerHTML = bio;
+
+    const cardim = document.querySelector(".imagem");
+    cardim.style.animation = "";
+    setTimeout(() => cardim.style.animation = "ani2 1s linear", 5);
+
+    const cardp = document.querySelector(".biog p");
+    cardp.style.animation = "";
+    setTimeout(() => cardp.style.animation = "pp 1s linear", 5);
+};
+function c2(){
+    document.getElementById("imc").src="Dorothy_Vaughan.png";
+    var nome = "Dorothy Vaughan"
+    document.getElementById("nc").innerHTML = nome;
+    var bio = "Dorothy Vaughan (20/09/1910 – 10/11/2008) foi uma das mais brilhantes mentes americanas na corrida espacial. Mesmo vivendo numa época onde o racismo era explícito nos Estados Unidos, ela fez a diferença, e foi uma das pioneiras na introdução dos computadores para cálculos de rota nos projetos da NASA, além de que utilizou a linguagem de programação Fortran (F).";
+    document.getElementById("sobc").innerHTML = bio;
+
+    const cardim = document.querySelector(".imagem");
+    cardim.style.animation = "";
+    setTimeout(() => cardim.style.animation = "ani2 1s linear", 5);
+
+    const cardp = document.querySelector(".biog p");
+    cardp.style.animation = "";
+    setTimeout(() => cardp.style.animation = "pp 1s linear", 5);
+};
+function c3(){
+    document.getElementById("imc").src="Evelyn_Boyd_Granville.png";
+    var nome = "Evelyn Boyd G."
+    document.getElementById("nc").innerHTML = nome;
+    
+    var bio = "Foi a primeira mulher negra na área da informática. Nasceu em 1924, formou-se em matemática pela universidade de Yela. Trabalhou como programadora de computadores para a IBM e contribuiu significativamente para o desenvolvimento de sistemas de computação e análise numérica. Após trabalhar na IBM, se tornou professora de matemática em várias universidades e foi uma defensora importante da educação e opurtunidades para a mulheres e minorias na área da tecnologia.";
+    document.getElementById("sobc").innerHTML = bio;
+
+    const cardim = document.querySelector(".imagem");
+    cardim.style.animation = "";
+    setTimeout(() => cardim.style.animation = "ani2 1s linear", 5);
+
+    const cardp = document.querySelector(".biog p");
+    cardp.style.animation = "";
+    setTimeout(() => cardp.style.animation = "pp 1s linear", 5);
+};
+function c4(){
+    document.getElementById("imc").src="Gerald_A._Lawson.png";
+    var nome = "Gerald A.Lawson"
+    document.getElementById("nc").innerHTML = nome;
+    var bio = "Se hoje em dia os gamers de plantão apertam o “pause” sem a preocupação de perder o jogo, podem agradecer ao engenheiro eletrônico norte-americano Gerald A. Lawson (que atendia também por Jerry Lawson). A invenção mais importante de Lawson, no entanto, foi o Fairchild Channel F, um sistema de videogame baseado em cartucho. Lawson fundou sua própria empresa, a VideoSoft, que produzia cartuchos para o Atari 2600. Sua morte aconteceu em 2011.";
+    document.getElementById("sobc").innerHTML = bio;
+
+    const cardim = document.querySelector(".imagem");
+    cardim.style.animation = "";
+    setTimeout(() => cardim.style.animation = "ani2 1s linear", 5);
+
+    const cardp = document.querySelector(".biog p");
+    cardp.style.animation = "";
+    setTimeout(() => cardp.style.animation = "pp 1s linear", 5);
+};
+function c5(){
+    document.getElementById("imc").src="Mark_Dean.png";
+    var nome = "Mark Dean"
+    document.getElementById("nc").innerHTML = nome;
+    
+    var bio = "O informático e inventor norte-americano conquistou espaço na indústria da tecnologia a partir do momento em que liderou a equipe que desenvolveu o Industry Standard Architecture (ISA), que permitiu a conexão entre computadores e periféricos (como a impressora), e a equipe de design que criou o primeiro chip processador de computador a giga-hertz. Além disso, Mark Dean detém três das nove patentes originais da IBM. Atualmente, está com 62 anos.";
+    document.getElementById("sobc").innerHTML = bio;
+
+    const cardim = document.querySelector(".imagem");
+    cardim.style.animation = "";
+    setTimeout(() => cardim.style.animation = "ani2 1s linear", 5);
+
+    const cardp = document.querySelector(".biog p");
+    cardp.style.animation = "";
+    setTimeout(() => cardp.style.animation = "pp 1s linear", 5);
+};
+function c6(){
+    document.getElementById("imc").src="Otis-Boykin.png";
+    var nome = "Otis Boykin"
+    document.getElementById("nc").innerHTML = nome;
+    
+    var bio = "O trabalho do inventor afro-americano Otis F. Boykin em resistores elétricos aprimorados tornou possível o funcionamento constante de uma variedade de dispositivos eletrônicos agora onipresentes. Variações de seus modelos de resistores são usadas hoje em todo o mundo em televisões, computadores e rádios. Mais notavelmente, porém, seu trabalho possibilitou funções de controle para o primeiro marca-passo implantável de sucesso.";
+    document.getElementById("sobc").innerHTML = bio;
+
+    const cardim = document.querySelector(".imagem");
+    cardim.style.animation = "";
+    setTimeout(() => cardim.style.animation = "ani2 1s linear", 5);
+
+    const cardp = document.querySelector(".biog p");
+    cardp.style.animation = "";
+    setTimeout(() => cardp.style.animation = "pp 1s linear", 5);
+};
+function c7(){
+    document.getElementById("imc").src="philip-Emeagwali.png";
+    var nome = "philip Emeagwali"
+    document.getElementById("nc").innerHTML = nome;
+    
+    var bio = "Obteve bacharelado em matemática e três outros diplomas: um doutorado em computação científica pela Universidade de Michigan e dois mestrados pela Universidade George Washington. Ele ficou conhecido na tecnologia por usar 65 mil processadores para inventar o computador mais rápido do mundo, que realiza 3,1 bilhões de cálculos por segundo. Ele foi eleito o 35º maior africano (e o maior cientista africano) de todos os tempos em uma pesquisa da revista New African.";
+    document.getElementById("sobc").innerHTML = bio;
+
+    const cardim = document.querySelector(".imagem");
+    cardim.style.animation = "";
+    setTimeout(() => cardim.style.animation = "ani2 1s linear", 5);
+
+    const cardp = document.querySelector(".biog p");
+    cardp.style.animation = "";
+    setTimeout(() => cardp.style.animation = "pp 1s linear", 5);
+};
+function c8(){
+    document.getElementById("imc").src="Sonia-Guimarães.png";
+    var nome = "Sonia Guimarães"
+    document.getElementById("nc").innerHTML = nome;
+    
+    var bio = "Foi a primeira negra brasileira doutora em Física pela University of Manchester Institute of Science and Technology, na Inglaterra. Atualmente, é especialista em Propriedade Eletróticas de Ligas Semicondutoras Crescidas Epitaxialmente, um campo que possibilitou o desenvolvimento de tecnologia para celulares.";
+    document.getElementById("sobc").innerHTML = bio;
+
+    const cardim = document.querySelector(".imagem");
+    cardim.style.animation = "";
+    setTimeout(() => cardim.style.animation = "ani2 1s linear", 5);
+
+    const cardp = document.querySelector(".biog p");
+    cardp.style.animation = "";
+    setTimeout(() => cardp.style.animation = "pp 1s linear", 5);
+};
+
+/* sobre */
+
+function sobre1(){
+    document.getElementById("ti").innerHTML = "Como surgiu a ideia?";
+    document.getElementById("p1").innerHTML = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, itaque nam delectus minima nostrum fugiat dolorum atque, recusandae, dolorem voluptate exercitationem neque ducimus aut reprehenderit iure voluptates eum quos quia!";
+    document.getElementById("p2").innerHTML = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, itaque nam delectus minima nostrum fugiat dolorum atque, recusandae, dolorem voluptate exercitationem neque ducimus aut reprehenderit iure voluptates eum quos quia!";
+    document.getElementById("p3").innerHTML = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, itaque nam delectus minima nostrum fugiat dolorum atque, recusandae, dolorem voluptate exercitationem neque ducimus aut reprehenderit iure voluptates eum quos quia!";
+    document.getElementById("p4").innerHTML = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, itaque nam delectus minima nostrum fugiat dolorum atque, recusandae, dolorem voluptate exercitationem neque ducimus aut reprehenderit iure voluptates eum quos quia!";
+    
+    document.getElementById("im").src="images4.jpg";
+
+    const anim = document.querySelector(".tela");
+    anim.style.animation = "";
+    setTimeout(() => anim.style.animation = "ani3 2s", 1);
+}
+function sobre2(){
+    document.getElementById("ti").innerHTML = "Nosso objetivo";
+
+    document.getElementById("p1").innerHTML = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero odio blanditiis at ipsam aliquam sint nisi. Voluptate voluptatem fuga quia magni error. Nihil voluptatum repudiandae fugiat totam, aperiam dolorem molestias.";
+    document.getElementById("p2").innerHTML = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero odio blanditiis at ipsam aliquam sint nisi. Voluptate voluptatem fuga quia magni error. Nihil voluptatum repudiandae fugiat totam, aperiam dolorem molestias.";
+    document.getElementById("p3").innerHTML = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero odio blanditiis at ipsam aliquam sint nisi. Voluptate voluptatem fuga quia magni error. Nihil voluptatum repudiandae fugiat totam, aperiam dolorem molestias.";
+    document.getElementById("p4").innerHTML = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero odio blanditiis at ipsam aliquam sint nisi. Voluptate voluptatem fuga quia magni error. Nihil voluptatum repudiandae fugiat totam, aperiam dolorem molestias.";
+    
+    document.getElementById("im").src = "images5.jpg";
+    
+    const anim = document.querySelector(".tela");
+    anim.style.animation = "";
+    setTimeout(() => anim.style.animation = "ani3 2s", 1);
+}
